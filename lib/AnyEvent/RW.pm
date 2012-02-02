87ca81234ca700831362290b0f6bb67c61c2f283
Mons Anderson <mons@cpan.org>
2012-02-02 12:42:53 +0400

    empty repo

---
+++ lib/AnyEvent/RW.pm
@@ -0,0 +1,60 @@
+package AnyEvent::RW;
+
+use 5.008008;
+use common::sense 2;m{
+use strict;
+use warnings;
+};
+use Carp;
+
+=head1 NAME
+
+AnyEvent::RW - ...
+
+=cut
+
+our $VERSION = '0.01'; $VERSION = eval($VERSION);
+
+=head1 SYNOPSIS
+
+    package Sample;
+    use AnyEvent::RW;
+
+    ...
+
+=head1 DESCRIPTION
+
+    ...
+
+=cut
+
+
+=head1 METHODS
+
+=over 4
+
+=item ...()
+
+...
+
+=back
+
+=cut
+
+sub new { ... }
+
+=head1 AUTHOR
+
+Mons Anderson, C<< <mons@cpan.org> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2012 Mons Anderson, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+=cut
+
+1;
